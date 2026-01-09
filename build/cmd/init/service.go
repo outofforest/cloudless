ac6fde8bae0a9fdb33ffc22db27e41dc91a353ef
Wojciech Malota-Wojcik <wojtek@exw.co>
2026-01-09 19:31:25 +0100

    Add ACME service

--- build/cmd/init/service.go
+++ build/cmd/init/service.go
@@ -2,8 +2,11 @@ package main
 
 import (
 	. "github.com/outofforest/cloudless" //nolint:staticcheck
+	"github.com/outofforest/cloudless/pkg/acme"
 	"github.com/outofforest/cloudless/pkg/container"
 	"github.com/outofforest/cloudless/pkg/dns"
+	dnsacme "github.com/outofforest/cloudless/pkg/dns/acme"
+	"github.com/outofforest/cloudless/pkg/pebble"
 	"github.com/outofforest/cloudless/pkg/shield"
 )
 
@@ -12,21 +15,45 @@ var HostService = Join(
 		Network("02:00:00:00:00:02", "eth0", Master("igw")),
 		Gateway("10.101.0.1"),
 		Bridge("igw", "02:00:00:00:02:01", IPs("10.101.0.2/24")),
-		container.New("dns", "/root/persistent/containers/cache",
+		container.New("dns", "/root/persistent/containers/dns",
 			container.Network("igw", "vdns", "02:00:00:00:02:02"),
+		),
+		container.New("pebble", "/root/persistent/containers/pebble",
+			container.Network("igw", "vpebble", "02:00:00:00:02:03"),
+		),
+		container.New("acme", "/root/persistent/containers/acme",
+			container.Network("igw", "vacme", "02:00:00:00:02:04"),
 		),
 	),
 	Container("dns",
 		Network("02:00:00:00:02:02", "igw", IPs("10.101.0.8/24")),
 		Gateway("10.101.0.1"),
 		shield.Open("udp4", "igw", dns.Port),
+		shield.Open("tcp4", "igw", dnsacme.Port),
 		dns.Service(
+			dns.ACME(),
 			dns.Zone("example.local", "ns1.example.local", "wojtek@exw.co", 1,
 				dns.Nameservers("ns1.example.local", "ns2.example.local"),
 				dns.Domain("ns1.example.local", "10.101.0.155"),
 				dns.Domain("ns2.example.local", "10.101.0.156"),
 				dns.Domain("test.example.local", "10.101.0.155"),
 			),
+		),
+	),
+	Container("pebble",
+		Network("02:00:00:00:02:03", "igw", IPs("10.101.0.9/24")),
+		Gateway("10.101.0.1"),
+		shield.Open("tcp4", "igw", pebble.Port),
+		pebble.Container("/root/persistent/apps/pebble", "10.101.0.8:53"),
+	),
+	Container("acme",
+		Network("02:00:00:00:02:04", "igw", IPs("10.101.0.10/24")),
+		Gateway("10.101.0.1"),
+		Mount("/root/persistent/apps/acme", "/acme", true),
+		shield.Open("tcp4", "igw", acme.Port),
+		acme.Service("/acme", "wojtek@exw.co", acme.Pebble("10.101.0.9"),
+			acme.DNSACMEs("10.101.0.8"),
+			acme.Domains("test.example.local"),
 		),
 	),
 )
